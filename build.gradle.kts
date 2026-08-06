d5a1e3ce8ed7c4ad0bffdf5fad3c5d274cd670b1
Drew Mecham <drew.m.mecham@gmail.com>
2026-08-05 21:34:33 -0600

    Restructure, cleanup UML, add PNGs of UML.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,4 +19,10 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showStandardStreams = true
+    }
 }
--- src/main/kotlin/state/regex/TokenRules.kt
+++ src/main/kotlin/state/regex/TokenRules.kt
@@ -3,15 +3,12 @@ package state.regex
 object TokenRules {
     private val digits = ('0'..'9').map { it.toString() }.toSet()
     private val nonZeroDigits = ('1'..'9').map { it.toString() }.toSet()
-    private val binaryDigits = setOf("0", "1")
     private val uppercase = ('A'..'Z').map { it.toString() }.toSet()
     private val specials = setOf("!", "@", "#", "$", "%", "&", "*")
 
     fun isDigit(token: String): Boolean = token in digits
 
     fun isNonZeroDigit(token: String): Boolean = token in nonZeroDigits
-
-    fun isBinaryDigit(token: String): Boolean = token in binaryDigits
 
     fun isUppercase(token: String): Boolean = token in uppercase
 
